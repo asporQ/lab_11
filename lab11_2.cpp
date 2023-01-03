1d8a76aab71d49f0c53595d659859814202aeba0
asporQ <tikump.aspor@gmail.com>
2023-01-03 15:33:59 +0700

    complete

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -3,25 +3,6 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-	ifstream source;
-	ofstream dest;
-	source.open("cheerbook.txt");
-	dest.open("cheerbook_copy.txt");
-	string textline;
-	dest << "-------------------- BOOM ---------------------\n";
-	getline(source, textline);
-	dest << "-------------------- HA!! ---------------------\n";
-
-	source.close();
-	dest.close();
-	return 0;
-}
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
 
 int main()
 {
